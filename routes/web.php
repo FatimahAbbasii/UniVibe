e19d46011c12e21ae3f21df352094e1fcb416147
Haha <abba0005@hz.nl>
2025-07-03 16:39:30 +0200

    Routes added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('mainPage');
 });
+Route::get('/map', function () {
+    return view('Map');
+});
+Route::get('/events', function () {
+    return view('Events');
+});
+Route::get('/profile', function () {
+    return view('Profile');
+});
