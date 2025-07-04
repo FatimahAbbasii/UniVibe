c984c9fd32c3f9cc52af413abbeea83ed5a4d920
Andrea-B3zz <andreabezzolato@Mac.zebi.nl>
2025-07-04 03:00:46 +0200

    Profile page fic

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', function () {
@@ -8,18 +9,21 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile', action: [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/', function () {
-    return view('mainPage');
+        return view('mainPage');
     });
     Route::get('/map', function () {
-    return view('Map');
+        return view('Map');
     });
     Route::get('/events', function () {
-    return view('Events');
+        return view('Events');
     });
+
+    Route::get('/createActivity', action: [Controller::class, 'create'])->name('createActivity');
+
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
